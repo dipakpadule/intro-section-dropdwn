fd6cb9bb31cd726ff7e14e146a160d0d47954ed9
dipak <sarkar3115@gmail.com>
2023-04-12 10:29:46 +0530

    desktop done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import adown from './images/icon-arrow-down.svg';
+import audiophile from './images/client-audiophile.svg';
+import databiz from './images/client-databiz.svg';
+import maker from './images/client-maker.svg';
+import meet from  './images/client-meet.svg';
+
+import todo from './images/icon-todo.svg';
+import calendar from './images/icon-calendar.svg';
+import reminder from './images/icon-reminders.svg';
+import planning from './images/icon-planning.svg';
+
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+
+  const [fdrop, setFdrop] = useState(false);
+  const [cdrop, setCdrop] = useState(false);
+  const handleFdrop = ()=>{
+    setFdrop(!fdrop);
+  };
+  const handleCdrop = ()=>{
+    setCdrop(!cdrop);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className='intro-section'>
+      <nav className='is-navbar'>
+        <div className='is-logo'>
+          <img src={logo} alt=''/>
+          <div  className='dropdown'>
+            <a href='/'>Features </a>
+            <img onClick={handleFdrop} src={adown} alt=''/>
+
+            <div  className={fdrop ? 'fdrop down':'fdrop'}>
+              <ul>
+                <li><img src={todo} alt=''/><a href='/'>ToDo list</a></li>
+                <li><img src={calendar} alt=''/><a href='/'>Calendar</a></li>
+                <li><img src={reminder} alt=''/><a href='/'>Reminders</a></li>
+                <li><img src={planning} alt=''/><a href='/'>Planning</a></li>
+              </ul>
+            </div>
+            <a href='/'>Company </a>
+            <img onClick={handleCdrop} src={adown} alt=''/>
+            <div className={cdrop ? 'cdrop down':'cdrop'}>
+              <ul>
+                <li><a href='/'>History</a></li>
+                <li><a href='/'>Our Team</a></li>
+                <li><a href='/'>Blog</a></li>
+              </ul>
+            </div>
+            <a href='/'>Careers</a>
+            <a href='/'>About</a>
+          </div>
+        </div>
+        <div className='is-login-btn'>
+          <button>Login</button>
+          <button id='reg'>Register</button>
+        </div>
+      </nav>
+      <section>
+          
+      </section>
+      <section className='is-main'>
+        <section className='is-main-info'>
+          <strong><span>Make </span> remote work</strong>
+          <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+          <button>Learn more</button>
+          <div className='clients'>
+            <img src={databiz} alt=''/>
+            <img src={audiophile} alt=''/>
+            <img src={meet} alt=''/>
+            <img src={maker} alt=''/>
+          </div>
+        </section>
+        <section className='is-main-bg'>
+
+        </section>
+      </section>
+    </article>
   );
 }
 
